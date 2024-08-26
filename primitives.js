6c7b3e7dfa39bf971cc4c191494c967025aa52f3
abu <maikoabraham68@gmail.com>
2024-08-26 11:47:49 +0300

    feat: creates a constan variable for the given four javascript primitive data types

--- primitives.js
+++ primitives.js
@@ -1,3 +1,10 @@
+/* Create a constant variable for each of the following primitive types:
+// str for string
+// num for number
+// bool for boolean
+// undef for undefined
+*/
+
 const str = String;
 const num = Number;
 const bool = Boolean;
