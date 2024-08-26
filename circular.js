1f2548fbe7e00fb649dfd265bd284cf45deeb37c
abrakingoo <maikoabraham68@gmail.com>
2024-08-26 17:17:15 +0300

    feat: Create an object named circular that has a property named circular with itself as the value.

--- circular.js
+++ circular.js
@@ -1,5 +1,6 @@
 // Create an object named circular that has a property named circular with itself as the value.
 
-const circular = {
-    circular
-}
+const circular = {}
+circular.circular = circular
+
+console.log(circular)
