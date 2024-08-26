16bbe253e5d30d315519b9d1de20cf5b3239c71b
abrakingoo <maikoabraham68@gmail.com>
2024-08-26 14:18:30 +0300

    feat :Create three copies of the person object named clone1, clone2 and samePerson.
    Increase the age of person by one, and set its country to 'FR'.

--- mutability.js
+++ mutability.js
@@ -6,12 +6,6 @@ Increase the age of person by one, and set its country to 'FR'.
 You must find a way to keep the original values of clone1 and clone2. The values of samePerson should change when person is changed.
 */
 
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  }
-
 person.age = 78;
 person.country = 'FR';
 
