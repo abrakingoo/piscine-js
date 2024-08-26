d5a6e93502cbe811a2bbb380bf70c6d9ef1d7dda
abrakingoo <maikoabraham68@gmail.com>
2024-08-26 17:45:36 +0300

    feat: creates two variables holding min and max number values

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -5,7 +5,5 @@ smalls with the smallest possible number value.
 biggie with the greatest possible number value.
 */
 
-const smalls = 5e-324;
-const biggie = 1.7976931348623157e+308;
-
-console.log(Number.MIN_VALUE)
+const smalls = Number.MIN_VALUE;
+const biggie = Number.MAX_VALUE;
