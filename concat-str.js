71182a202e4cdab6cc44c7d9125eea2e1ea1c1ee
abrakingoo <maikoabraham68@gmail.com>
2024-08-26 14:54:49 +0300

    feat: a function concatStr that returns its arguments concatinated

--- concat-str.js
+++ concat-str.js
@@ -1,2 +1,2 @@
 // Create a function named concatStr which takes 2 arguments and concatenates them.
-const concatStr = (str1,  str2) => str1 + str2
+const concatStr = (str1,  str2) => String(str1) + str2
