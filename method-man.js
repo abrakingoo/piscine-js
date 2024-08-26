957884ffe46fb57a1b23416a9a376a8b33a2de03
abrakingoo <maikoabraham68@gmail.com>
2024-08-26 18:25:47 +0300

    words: that takes a string, splits it by spaces, and returns an array of those substrings. sentence: that takes an array of strings, and joins them with spaces to return a single string. yell: that takes a string and returns it in upper case. whisper: that takes a string and returns it in lower case, surrounded by *. capitalize: that takes a string and transforms it so that the first character is upper case, and the subsequent characters are lower case.

--- method-man.js
+++ method-man.js
@@ -9,10 +9,10 @@ capitalize: that takes a string and transforms it so that the first character is
 and the subsequent characters are lower case.
 */
 
-const words = (str) => str.split('');
+const words = (str) => str.split(' ');
 const sentence = (arr) => arr.join(" ");
 const yell = (str) => str.toUpperCase();
 const whisper = (str) => '*' + str.toLowerCase() + "*";
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
-console.log(whisper("HELLO"))
+console.log(words("H E L L O"))
