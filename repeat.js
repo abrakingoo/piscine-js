487a0e52c89776b09711534bc6463ff5011bde40
abrakingoo <maikoabraham68@gmail.com>
2024-08-28 12:44:43 +0300

    feat: a function named repeat that takes a string and a number as arguments
    returns the string repeated as many times as the number describes.
    It should function like String.repeat(), but of course you must make your own.

---
+++ repeat.js
@@ -0,0 +1,24 @@
+/*
+Create a function named repeat that takes a string and a number as arguments,
+and returns the string repeated as many times as the number describes.
+It should function like String.repeat(), but of course you must make your own.
+*/
+
+const repeat = (str, n) => {
+
+    if (n < 0 || n > Number.POSITIVE_INFINITY) {
+        return "RangeError";
+    }
+
+    let st = ""
+    for (let i = 0; i < Math.floor(n); i++) {
+        st += str;
+
+        if (i < n -1) {
+            st += " ";
+        }
+    }
+    return st
+}
+
+console.log(repeat("hello", 3))
