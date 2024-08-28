bd6c6de0116159378c2bf0dc10c30610416dfce0
abrakingoo <maikoabraham68@gmail.com>
2024-08-28 17:31:55 +0300

    refactor: handles instance where str len is less the 2

--- keep-cut.js
+++ keep-cut.js
@@ -19,6 +19,6 @@ const cutLast = (str) => str.slice(0,-2);
 const cutFirstLast = (str) => cutLast(cutFirst(str));
 const keepFirst = (str) => str.slice(0, 2);
 const keepLast = (str) => str.slice(-2);
-const keepFirstLast = (str) => keepFirst(str) + keepLast(str);
+const keepFirstLast = (str) => str.length <= 2 ? str : keepFirst(str) + keepLast(str);
 
-// console.log(keepFirstLast('hello'));
+console.log(keepFirstLast('he'));
