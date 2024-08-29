bb22d7ed96e54d49e88ce0e0ece0fd7a6a84e495
abrakingoo <maikoabraham68@gmail.com>
2024-08-29 15:21:43 +0300

    core: creates the round and ceil functions

---
+++ cut-corners.js
@@ -0,0 +1,75 @@
+/*
+Create some functions which behave like JavaScript's Math rounding functions:
+
+round: which behaves similar to Math.round().
+ceil: which behaves similar to Math.ceil().
+floor: which behaves similar to Math.floor().
+trunc: which behaves similar to Math.trunc().
+*/
+
+const modulo = (a, b) => {
+
+    if (b === 0) {
+        return
+    }
+    
+    let isNegative = (a < 0);
+
+    a = Math.abs(a);
+    b = Math.abs(b);
+
+    while(a >= b) {
+        a -= b;
+    }
+
+    return isNegative ? -a : a;
+}
+
+const round = (x) => {
+    let neg = false;
+
+    if (x === Number.POSITIVE_INFINITY) return Infinity;
+    if (x === Number.NEGATIVE_INFINITY) return -Infinity;
+
+    if (x < 0) {
+        x = Math.abs(x);
+        neg = true;
+    }
+    
+    if (modulo(x, 1) >= 0.5 ) {
+        let val = x - (modulo(x, 1)) + 1
+        return neg ? -val : val;
+    }
+
+    return neg ? -(x - modulo(x, 1)) : x - modulo(x, 1);
+}
+
+const ceil = (x) => {
+    let neg = false;
+
+    if (x === 0) return 0;
+
+    if (x === Number.POSITIVE_INFINITY) return Infinity;
+    if (x === Number.NEGATIVE_INFINITY) return -Infinity;
+
+    if (x < 0) {
+        x = Math.abs(x);
+        neg = true;
+    }
+
+    if (x < 0 && neg) return -0;
+
+    if (modulo(x, 1) >= 0.5 ) {
+        let val = x - (modulo(x, 1)) + 1
+        return neg ? -val : val;
+    }
+
+    return neg ? -(x - modulo(x, 1)) : x - modulo(x, 1);
+
+}
+
+// const nums = [3.7, -3.7, 3.1, -3.1]
+// // console.log(nums.map(round))
+
+// console.log(nums.map(ceil))
+// console.log(Math.ceil(3.7))
