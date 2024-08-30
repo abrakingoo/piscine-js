d809e756cac8397a604dde33a5df7c59b9d5b92f
abrakingoo <maikoabraham68@gmail.com>
2024-08-30 17:40:38 +0300

    update: fixed the split function

--- unbreakable.js
+++ unbreakable.js
@@ -10,7 +10,7 @@ const split = (str, separator) => {
     let arr = [];
 
     if (str.length === 0 && separator.length > 0) {
-        return arr;
+        return arr[''];
     }
 
     if (str.length === 1) {
@@ -40,26 +40,25 @@ const split = (str, separator) => {
 
 const join = (arr, separator) => {
     let str = "";
+
     if (arr.length < 1) {
         return str;
     }
 
-    if (separator === "") {
-        for (elem of arr) {
-            str += elem;
-        }
-        return str;
-    }
-
     for (let i = 0; i < arr.length; i++) {
-        i < arr.length-1 ? str += arr[i] += separator  : str += arr[i];
+        str += arr[i];
+        if (i < arr.length - 1) {
+            str += separator;
+        }
     }
+    
     return str;
 }
+
 
 // const str = 'The quick brown fox jumps over the lazy dog.';
 // console.log(split('ggg - ddd - b', ' - '))
 // console.log(split('a b c', ' '))
-console.log(split('ee,ff,g,', ','))
+// console.log(split('ee,ff,g,', ','))
 // console.log([].join(" "))
 // console.log(join(["hello", "world", "How", "Is", "This", "Text"], ","))
