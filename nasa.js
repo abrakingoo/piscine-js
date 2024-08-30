d302f999e81a23eb4cd6c0f1086e6d4294524c49
abrakingoo <maikoabraham68@gmail.com>
2024-08-30 18:02:08 +0300

    core: Creates a function named nasa that takes a number N as an argument and returns a string  with all numbers from 1 to N separated by spaces. There are three exceptions:
    Convert numbers which are divisible by 3 to "NA".
    Convert numbers which are divisible by 5 to "SA".
    Convert numbers which are divisible by 3 and 5 to "NASA".

---
+++ nasa.js
@@ -0,0 +1,34 @@
+/*
+Create a function named nasa that takes a number N as an argument and returns a string
+ with all numbers from 1 to N separated by spaces. There are three exceptions:
+
+Convert numbers which are divisible by 3 to "NA".
+Convert numbers which are divisible by 5 to "SA".
+Convert numbers which are divisible by 3 and 5 to "NASA".
+*/
+
+const nasa = (n) => {
+    let str = "";
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            str += "NASA";
+            continue;
+        }
+
+        if (i % 5 === 0) {
+            str += "SA";
+            continue;
+        }
+
+        if (i % 3 === 0)  {
+            str += "NA";
+            continue;
+        }
+
+        str += i;
+    }
+
+    return str;
+}
+
+// console.log(nasa(15))
