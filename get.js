f4cb23b3c72aaa787e70023d7b34e5e70fd742f2
abrakingoo <maikoabraham68@gmail.com>
2024-08-30 18:38:02 +0300

    core: function will return the value at the given path in the src object.

---
+++ get.js
@@ -0,0 +1,14 @@
+/*
+Create a function named get which takes two arguments:
+
+src: an object.
+path: a string representing a path.
+Your function will return the value at the given path in the src object.
+
+Example:
+const src = { nested: { key: 'peekaboo' } }
+const path = 'nested.key'
+get(src, path) // -> 'peekaboo'
+*/
+
+const get = (src, path) => path;
