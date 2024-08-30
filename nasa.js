8a04dbacf6730ec078cb3f940032b7e44b5d4ab6
abrakingoo <maikoabraham68@gmail.com>
2024-08-30 18:07:50 +0300

    update: fixes the spacing between the sring elements

--- nasa.js
+++ nasa.js
@@ -12,23 +12,37 @@ const nasa = (n) => {
     for (let i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             str += "NASA";
+            if (i < n){
+                str += " ";
+            }
             continue;
+            
         }
 
         if (i % 5 === 0) {
             str += "SA";
+            if (i < n){
+                str += " ";
+            }
             continue;
         }
 
         if (i % 3 === 0)  {
             str += "NA";
+            if (i < n){
+                str += " ";
+            }
             continue;
         }
 
+        
         str += i;
+        if (i < n){
+            str += " ";
+        }
     }
 
     return str;
 }
 
-// console.log(nasa(15))
+console.log(nasa(15))
