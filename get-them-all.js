d495ae1dccd42ab94b8655f9ac8af1cdddb153c5
abrakingoo <maikoabraham68@gmail.com>
2024-09-05 13:54:21 +0300

    Add: get-them-all solution

--- get-them-all.js
+++ get-them-all.js
@@ -1,23 +1,10 @@
-/*
-Write the body of the getArchitects function, which returns an array containing 2 arrays of HTML elements:
-
-the first array contains the architects, all corresponding to a <a> tag
-the second array contains all the non-architects people
-*/
 export const getArchitects = () => {
     return [
-        document.getElementsByTagName("a"),
-        document.querySelectorAll("body span")
+        document.querySelectorAll("body a"),
+        document.querySelectorAll("body span"),
     ];
 }
 
-
-/*
-Write the body of the getClassical function, which returns an array containing 2 arrays of HTML elements:
-
-the first array contains the architects belonging to the classical class
-the second array contains the non-classical architects
-*/
 export const getClassical = () => {
     return [
         document.querySelectorAll("a.classical"),
@@ -25,12 +12,6 @@ export const getClassical = () => {
     ];
 }
 
-/*
-Write the body of the getActive function, which returns an array containing 2 arrays of HTML elements:
-
-the first array contains the classical architects who are active in their class
-the second array contains the non-active classical architects
-*/
 export const getActive = () => {
     return [
         document.querySelectorAll("a.classical.active"),
@@ -38,16 +19,9 @@ export const getActive = () => {
     ];
 }
 
-
-/*
-Write the body of the getBonannoPisano function, which returns an array containing:
-
-the HTML element of the architect you're looking for, whose id is BonannoPisano
-an aarray which contains all the remaining HTML elements of active classical architects
-*/
 export const getBonannoPisano = () => {
     return [
-        document.getElementById("#BonannoPisano"),
+        document.getElementById("BonannoPisano"),
         document.querySelectorAll("a.classical.active"),
     ];
 }
